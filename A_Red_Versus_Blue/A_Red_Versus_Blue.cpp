73dc8af214e917cc91302625a7a8cfc7c6e3ce47
Jitesh117 <sahookumarjitesh@gmail.com>
2022-05-22 12:22:41 +0530

    added 782 div 2 redvsblue

--- A_Red_Versus_Blue/A_Red_Versus_Blue.cpp
+++ A_Red_Versus_Blue/A_Red_Versus_Blue.cpp
@@ -5,14 +5,15 @@ typedef vector<int> vi;
 
 void solve()
 {
-   int n,r,b;
-   cin >> n >> r >> b;
-   int p = r/(b+1),q=r%(b+1);
-   for(int i = 0;i<q;i++)
-        cout << string(p+1,'R')<< 'B';
-    for(int i = q;i<b;i++)
-        cout << string(p,'R') << 'B';
-    cout << string(p,'R')<< "\n";
+    int n,r,b;
+    cin >> n >> r >> b;
+    int p=r/(b+1),q=r%(b+1);
+    for(int i=0;i<q;i++)   
+        cout<<string(p+1,'R')<<'B';
+    for(int i=q;i<b;i++)
+        cout<<string(p,'R')<<'B';
+    cout<<string(p,'R');
+    cout<<endl;
 }
 
 int main()
