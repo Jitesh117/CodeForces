24123c2bf517c680a597b21b38714098731bf8a5
Jitesh117 <sahookumarjitesh@gmail.com>
2022-05-29 08:39:56 +0530

    added maximum gcd problem

---
+++ A_Maximum_GCD/A_Maximum_GCD.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+
+void solve()
+{
+    int n =1;
+    cin >> n;
+    cout << n/2 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        solve();
+    }
+    return 0;
+}
