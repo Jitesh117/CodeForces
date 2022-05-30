aa793381bd6630d5f48f02b91197eac3d6835d0b
Jitesh117 <sahookumarjitesh@gmail.com>
2022-05-30 19:23:43 +0530

    added new year and sphere transformation

--- C_New_Year_and_the_Sphere_Transmission/C_New_Year_and_the_Sphere_Transmission.cpp
+++ C_New_Year_and_the_Sphere_Transmission/C_New_Year_and_the_Sphere_Transmission.cpp
@@ -5,9 +5,23 @@ typedef vector<int> vi;
 
 void solve()
 {
-    int n; 
-    cin >> n;
-    
+   ll N; cin >> N;
+	vector<ll> ans;
+
+	for (ll i = 1; i*i <= N; ++i) {
+		if (N%i==0) {
+			ans.push_back(N*(i-1)/2 + i);
+			if (i*i!=N) {
+				ans.push_back(N*(N/i-1)/2 + N/i);
+			}
+		}
+	}
+	sort(ans.begin(),ans.end());
+	
+	for (int i = 0; i < ans.size(); ++i) {
+		cout << ans[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
