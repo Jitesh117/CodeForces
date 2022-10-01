2c5ff788b3f34bcd899690493b35008200ddb71a
Jitesh117 <sahookumarjitesh@gmail.com>
2022-10-01 11:52:29 +0530

    added prime and factor functions to template

--- template.cpp
+++ template.cpp
@@ -1,8 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<long long> vl;
+
+bool isprime(int x)
+{
+    long long n = INT_MAX;
+    vector<int> v(n + 1, 1);
+    for (int i = 2; i <= n; i++)
+    {
+        if (v[i])
+            continue;
+        for (int j = i * 2; j <= n; j += i)
+        {
+            v[j] = 0;
+        }
+    }
+    return v[x];
+}
+
+vector<int> factor(int n)
+{
+    vector<int> f;
+    for (int x = 2; x * x <= n; x++)
+    {
+        while (n % x == 0)
+        {
+            f.push_back(x);
+            n /= x;
+        }
+    }
+    if (n > 2)
+        f.push_back(n);
+    return f;
+}
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return (b, a % b);
+}
+
+int modpow(int x, int n, int m)
+{
+    if (n == 0)
+        return 1 % m;
+    long long u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1)
+        u = (u * x) % m;
+    return u;
+}
+
 void solve()
 {
     
@@ -12,10 +63,10 @@ int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-    
+
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         solve();
     }
