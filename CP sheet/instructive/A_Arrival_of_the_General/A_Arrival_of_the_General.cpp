fee780de700439b3f00af5d04ba09ee44330f67b
Jitesh117 <sahookumarjitesh@gmail.com>
2023-08-30 15:01:16 +0530

    did some more instructive problems

---
+++ CP sheet/instructive/A_Arrival_of_the_General/A_Arrival_of_the_General.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+#define nline "\n"
+#define pb push_back
+#define PI 3.141592653589793238462
+
+bool isprime(int x)
+{
+    long long n = INT_MAX;
+    vector<int> v(n + 1, 1);
+    for (int i = 2; i <= n; i++)
+    {
+        if (v[i])
+            continue;
+        for (int j = i * 2; j <= n; j += i)
+        {
+            v[j] = 0;
+        }
+    }
+    return v[x];
+}
+
+vector<int> factor(int n)
+{
+    vector<int> f;
+    for (int x = 2; x * x <= n; x++)
+    {
+        while (n % x == 0)
+        {
+            f.push_back(x);
+            n /= x;
+        }
+    }
+    if (n > 2)
+        f.push_back(n);
+    return f;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b > a)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+int modpow(int x, int n, int m)
+{
+    if (n == 0)
+        return 1 % m;
+    long long u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1)
+        u = (u * x) % m;
+    return u;
+}
+int max(int a, int b, int c)
+{
+    return max(a, max(b, c));
+}
+int min(int a, int b, int c)
+{
+    return min(a, min(b, c));
+}
+ll max(ll a, ll b, ll c)
+{
+    return max(a, max(b, c));
+}
+ll min(ll a, ll b, ll c)
+{
+    return min(a, min(b, c));
+}
+
+void solve()
+{
+    int n, a[100];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int small = 100;
+    int big = 0;
+    int bigpos, spos;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > big)
+        {
+            big = a[i];
+            bigpos = i;
+        }
+        if (a[i] <= small)
+        {
+            small = a[i];
+            spos = i;
+        }
+    }
+    if (bigpos > spos)
+        cout << (bigpos) + (n - spos - 1) - 1;
+    else
+        cout << (bigpos + n - spos - 1);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    solve();
+    return 0;
+}
---
+++ CP sheet/instructive/A_Boring_Apartments/A_Boring_Apartments.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+
+#define nline  "\n"
+#define pb push_back
+#define PI 3.141592653589793238462
+
+bool isprime(int x)
+{
+    long long n = INT_MAX;
+    vector<int> v(n + 1, 1);
+    for (int i = 2; i <= n; i++)
+    {
+        if (v[i])
+            continue;
+        for (int j = i * 2; j <= n; j += i)
+        {
+            v[j] = 0;
+        }
+    }
+    return v[x];
+}
+
+vector<int> factor(int n)
+{
+    vector<int> f;
+    for (int x = 2; x * x <= n; x++)
+    {
+        while (n % x == 0)
+        {
+            f.push_back(x);
+            n /= x;
+        }
+    }
+    if (n > 2)
+        f.push_back(n);
+    return f;
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b > a)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+int modpow(int x, int n, int m)
+{
+    if (n == 0)
+        return 1 % m;
+    long long u = modpow(x, n / 2, m);
+    u = (u * u) % m;
+    if (n % 2 == 1)
+        u = (u * x) % m;
+    return u;
+}
+int max(int a, int b, int c)
+{
+    return max(a, max(b, c));
+}
+int min(int a, int b, int c)
+{
+    return min(a, min(b, c));
+}
+ll max(ll a, ll b, ll c){
+    return max(a, max(b, c));
+}
+ll min(ll a, ll b, ll c){
+    return min(a, min(b, c));
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    while(n--){
+        int result = 0;
+        cin >> s;
+        result += (s[0] - '0' - 1) * 10;
+        int t = s.size();
+        while(t--){
+            result += t + 1;
+        }
+        cout << result << nline;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    solve();
+    return 0;
+}
